1d75d56cd2fdc7e8484b652eb45a4c2dd287d4f9
aditya nehra <asniii.coder@gmail.com>
2018-09-11 00:05:13 +0530

    properties_java

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,8 +1,17 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.aditya"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 }
--- src/main/java/com/aditya/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/aditya/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.aditya.repository;
 
 import com.aditya.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,12 +10,16 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("dbUsername")
+    private String dbUsername;
+
     public List<Customer> findAll(){
         List<Customer> customers = new ArrayList();
         Customer customer = new Customer();
         customer.setFirstname("aditya");
         customer.setLastname("nehra");
         customers.add(customer);
+        System.out.println("dbusername is " + dbUsername);
         return customers;
     }
 }
